b425d78bdbfcd627918f82671425c0d7dbfa2f8d
Daniel <danielmordelfer@gmail.com>
2026-01-26 17:31:07 +0000

    new background for the screens

--- Assets/DeactivateObject.cs
+++ Assets/DeactivateObject.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class DeactivateObject : MonoBehaviour
 {
+    [SerializeField] private GameObject otherObject;
+
+  
     public void Deactivate() => gameObject.SetActive(false);
+
+    
+    public void ActivateOther()
+    {
+        if (otherObject != null)
+            otherObject.SetActive(true);
+    }
+
+    
+    public void DeactivateOther()
+    {
+        if (otherObject != null)
+            otherObject.SetActive(false);
+    }
 }
