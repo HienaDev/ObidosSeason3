104608280cdf7630cc918dfafc14c0fec2e07ae6
Daniel <danielmordelfer@gmail.com>
2026-02-23 23:02:43 +0000

    new fixes

--- Assets/Scripts/Localization/Integration/LocalizedTMPro.cs
+++ Assets/Scripts/Localization/Integration/LocalizedTMPro.cs
@@ -9,19 +9,55 @@ namespace LocalizationSystem.Integration
     {
         private TMP_Text _tmPro;
 
+        [Header("Layout tweak (optional)")]
+        [SerializeField] private RectTransform targetToMove;   
+        [SerializeField] private float ptMoveX = 0f;           
+
+        private bool _cachedDefault;
+        private Vector2 _defaultAnchoredPos;
+
         private void Awake()
         {
             _tmPro ??= GetComponent<TMP_Text>();
+
+         
+            if (targetToMove == null)
+                targetToMove = GetComponent<RectTransform>();
+
+            CacheDefaultPos();
+        }
+
+        private void CacheDefaultPos()
+        {
+            if (_cachedDefault || targetToMove == null) return;
+            _defaultAnchoredPos = targetToMove.anchoredPosition;
+            _cachedDefault = true;
         }
 
         protected override void Localize(LocalizedInfo info)
         {
             _tmPro.text = info.Text;
+
+            CacheDefaultPos();
+
+            if (targetToMove == null || !_cachedDefault)
+                return;
+
+            
+            targetToMove.anchoredPosition = _defaultAnchoredPos;
+
+           
+            if (info.Language == Language.pt_pt)
+            {
+                targetToMove.anchoredPosition = _defaultAnchoredPos + new Vector2(ptMoveX, 0f);
+            }
         }
 
-        void Reset()
+        private void Reset()
         {
             _tmPro = GetComponent<TMP_Text>();
+            targetToMove = GetComponent<RectTransform>();
+
             if (_tmPro)
             {
                 _localizations = new LocalizedInfo[1];
@@ -30,4 +66,3 @@ namespace LocalizationSystem.Integration
         }
     }
 }
-
